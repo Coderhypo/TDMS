e555914944ea152ff7727c25f874d7d79fc759e3
Coderhypo <i@ihypo.net>
2015-12-01 10:57:33 +0800

    update app __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,24 @@
 __author__ = 'hypo'
+
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.pagedown import PageDown
+
+import os
+from config import config
+
+app = Flask(__name__)
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+pagedown = PageDown()
+
+with app.app_context():
+    config_name = os.getenv('CONFIG') or 'default'
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    login_manager.init_app(app)
