31d24af8ff1d5194b1cf6d34732faa6997f97025
Coderhypo <i@ihypo.net>
2015-12-03 02:26:15 +0800

    update template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 __author__ = 'hypo'
 
 from app import app
@@ -26,34 +27,35 @@ def admin():
 
 @app.route('/admin/lend')
 def lend():
-    return render_template('/admin/index.html')
+
+    return render_template('/admin/devices/lenddevice.html')
 
 
 @app.route('/admin/return')
 def reDevices():
-    return render_template('/admin/index.html')
+    return render_template('/admin/devices/returndevice.html')
 
 
 @app.route('/admin/devices')
 def devices():
-    return render_template('/admin/index.html')
+    return render_template('/admin/devices/devicemanage.html')
 
 
 @app.route('/admin/users')
 def users():
-    return render_template('/admin/index.html')
+    return render_template('/admin/users/usermanage.html')
 
 
 @app.route('/admin/lendlogs')
 def lendLogs():
-    return render_template('/admin/index.html')
+    return render_template('/admin/logs/lendlogs.html')
 
 
 @app.route('/admin/schools')
 def schools():
-    return render_template('/admin/index.html')
+    return render_template('/admin/users/schoolmanage.html')
 
 
 @app.route('/admin/logs')
 def logs():
-    return render_template('/admin/index.html')
+    return render_template('/admin/logs/logmanage.html')
