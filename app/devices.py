5eec131bf689c4794d44284d7979832ce3df140b
Coderhypo <i@ihypo.net>
2015-12-07 09:51:25 +0800

    update

--- app/devices.py
+++ app/devices.py
@@ -4,7 +4,19 @@ from app.models import Devices
 
 __author__ = 'hypo'
 
-class DeviceInfo():
+
+class DeviceInfo:
+
+    """
+    设备信息类
+    用来创建新设备信息记录
+    getNewDevice 获得新设备并持久化
+    updateDevice 更新设备信息
+    deleteDevice 删除设备
+    """
+
+    def __init__(self):
+        pass
 
     __device_name = 'NONE'
     __device_type_id = 1
@@ -18,14 +30,13 @@ class DeviceInfo():
     def setStatus(self, status):
         self.__device_status = status
 
-    def setSchool(self,schoolid):
+    def setSchool(self, schoolid):
         self.__school_id = schoolid
 
     def setLendLogId(self, logid):
         self.__lend_log_id = logid
 
-    def getDevice(self):
-
+    def getNewDevice(self):
         device = Devices()
         device.device_name = self.__device_name
         device.device_type_id = self.__device_type_id
@@ -38,15 +49,41 @@ class DeviceInfo():
 
         return device
 
+    def updateDevice(self, id):
+        device = Devices.query.filter_by(device_id=id).first()
 
-class Device():
+        device.device_name = self.__device_name
+        device.device_type_id = self.__device_type_id
+        device.device_status = self.__device_status
+        device.school_id = self.__school_id
+        device.lend_log_id = self.__lend_log_id
+
+        db.session.add(device)
+        db.session.commit()
+
+    def deleteDevice(self, id):
+        device = Devices.query.filter_by(device_id=id).first()
+
+        db.session.delete(device)
+        db.session.commiit()
+
+
+class Device:
+
+    """
+    设备类
+    用来处理借出，归还等业务逻辑
+    lendDevice 借出设备
+    returnDevice 归还设备
+    """
 
     __device = 0
 
     def __init__(self, id):
         self.__device = Devices.query.filter_by(device_id=id).first()
 
-    def __init__(self, deviceinfo):
-        self.__device = deviceinfo.getDevice()
+    def lendDevice(self, lender, doer):
+        pass
 
-
+    def returnDevice(self, lender, doer):
+        pass
