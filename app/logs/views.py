727d5f570f76ffd27cc9b40913cb03a7ee1b3fc0
Coderhypo <i@ihypo.net>
2015-12-08 14:24:28 +0800

    update app/logs/views.py

--- app/logs/views.py
+++ app/logs/views.py
@@ -1,1 +1,21 @@
+# coding=utf-8
+from app import app
+from flask import render_template
+
 __author__ = 'hypo'
+
+
+@app.route('/admin/lendlogs')
+def lendLogs():
+
+    """设备借出归还日志"""
+
+    return render_template('/admin/logs/lendlogs.html')
+
+
+@app.route('/admin/logs')
+def logs():
+
+    """系统日志 管理员权限"""
+
+    return render_template('/admin/logs/logmanage.html')
