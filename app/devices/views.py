73c8e0bda5f672416357e02d0cb824fc616ee3b7
Coderhypo <i@ihypo.net>
2015-12-08 14:23:54 +0800

    update devices/views

--- app/devices/views.py
+++ app/devices/views.py
@@ -5,38 +5,6 @@ from flask import render_template
 __author__ = 'hypo'
 
 
-@app.route('/')
-def index():
-
-    """首页"""
-
-    return render_template('base.html')
-
-
-@app.route('/login')
-def login():
-
-    """登录"""
-
-    return render_template('base.html')
-
-
-@app.route('/logout')
-def logout():
-
-    """登出"""
-
-    return render_template('base.html')
-
-
-@app.route('/admin')
-def admin():
-
-    """后台主页"""
-
-    return render_template('/admin/index.html')
-
-
 @app.route('/admin/lend')
 def lend():
 
@@ -60,34 +28,3 @@ def devices():
 
     return render_template('/admin/devices/devicemanage.html')
 
-
-@app.route('/admin/users')
-def users():
-
-    """用户管理界面"""
-
-    return render_template('/admin/users/usermanage.html')
-
-
-@app.route('/admin/lendlogs')
-def lendLogs():
-
-    """设备借出归还日志"""
-
-    return render_template('/admin/logs/lendlogs.html')
-
-
-@app.route('/admin/schools')
-def schools():
-
-    """学院管理 管理员权限"""
-
-    return render_template('/admin/users/schoolmanage.html')
-
-
-@app.route('/admin/logs')
-def logs():
-
-    """系统日志 管理员权限"""
-
-    return render_template('/admin/logs/logmanage.html')
