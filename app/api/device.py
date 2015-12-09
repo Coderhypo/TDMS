bc556124729e33fdb033c852b138c428207c5b5b
Coderhypo <i@ihypo.net>
2015-12-09 17:56:57 +0800

    update device api

--- app/api/device.py
+++ app/api/device.py
@@ -7,22 +7,28 @@ from flask import request
 __author__ = 'hypo'
 
 
-@app.route('/api/device', methods=['GET'])
-def device():
+@app.route('/api/device/<int:deviceid>', methods=['GET'])
+def device(deviceid):
     rnt = {}
 
     if request.method == 'GET':
-        deviceid = request.args.get('did', 0)
-        status = request.args.get('sid', 0)
 
         if deviceid != 0:
             device = Devices.query.filter_by(device_id=deviceid).first()
             if device is not None:
                 rnt = device.to_json()
 
+    return json.dumps(rnt)
+
+
+@app.route('/api/devices/<int:status>', methods=['GET'])
+def devicestatus(status):
+    rnt = []
+
+    if request.method == 'GET':
+
         if status != 0:
             devices = Devices.query.filter_by(device_status=status).all()
-            rnt = []
             for device in devices:
                 tmp = device.to_json()
                 rnt.append(tmp)
