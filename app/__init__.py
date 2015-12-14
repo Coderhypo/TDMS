7538cb8a7bb3eaa6464e7ba4fb34f66fe83d0e5c
Coderhypo <i@ihypo.net>
2015-12-14 20:33:28 +0800

    update user login

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ import os
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
+from flask.ext.login import LoginManager
 from config import config
 
 __author__ = 'hypo'
@@ -11,12 +11,22 @@ __author__ = 'hypo'
 app = Flask(__name__)
 db = SQLAlchemy(use_native_unicode="utf8")
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+
 with app.app_context():
     config_name = os.getenv('CONFIG') or 'default'
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     db.init_app(app)
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(userid):
+        return Users.query.filter_by(user_id=userid).first()
+
 
 from .views import *
 from .devices import *
