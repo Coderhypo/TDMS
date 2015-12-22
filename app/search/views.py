014b42f1de07221877f928b0fa7b143e4d494d99
Coderhypo <i@ihypo.net>
2015-12-22 14:22:34 +0800

    create search view

--- app/search/views.py
+++ app/search/views.py
@@ -1,1 +1,14 @@
+# coding=utf-8
+from app import app
+from flask import render_template, request
+from flask.ext.login import login_required, current_user
+
+from app.models import Devices, Users, LendLogs
+
 __author__ = 'hypo'
+
+
+@app.route('/admin/search')
+@login_required
+def search():
+    pass
