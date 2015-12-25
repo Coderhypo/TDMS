1cb54a4870f60326c4852aacfc623a3ecaea7c24
Coderhypo <i@ihypo.net>
2015-12-25 16:03:12 +0800

    use flask manage

--- manage.py
+++ manage.py
@@ -1,5 +1,10 @@
 # -*- coding=utf-8 -*-
 from app import app
 
+from flask.ext.script import Manager
+from app import app
+
+manager = Manager(app)
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
